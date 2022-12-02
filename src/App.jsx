f9f5e193147e0884f78b575b1a2e98cf8ba04abc
bozenakr <bozena.krasevic@gmail.com>
2022-12-02 13:51:25 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
 function App() {
     return (
         <div className="App">
-            <div className="App-header" style={{ flexDirection: 'row' }}>
+            <div className="App-header">
                 {dogs.map((dog, i) => (
                     <div key={i} className="square">
                         {dog}
